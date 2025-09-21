78ba2d4e7d848b8300abcb5f46a7b3aeac363496
Sujay <worker.station.10@gmail.com>
2025-09-21 21:26:10 +0530

    removed "home" endpoint from swagger

--- fastapi_server.py
+++ fastapi_server.py
@@ -1,10 +1,15 @@
 from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-
+from fastapi.responses import HTMLResponse
 from app.services import stock_service, company_service, news_service, calendar_service, economic_service
 
 app = FastAPI(title="Financial Microservice Project")
+
+# Home page route
+@app.get("/", response_class=HTMLResponse, include_in_schema=False)
+def home(request: Request, ):
+    return templates.TemplateResponse("index.html", {"request": request})
 
 # Include routers
 app.include_router(stock_service.router, prefix="/market", tags=["Stock Market"])
@@ -16,13 +21,6 @@ app.include_router(economic_service.router, prefix="/economic", tags=["Economic 
 # Setup templates
 templates = Jinja2Templates(directory="app/templates")
 
-# # Mount static files (optional if you have CSS/JS)
-# app.mount("/static", StaticFiles(directory="app/static"), name="static")
-
-# Home page route
-@app.get("/")
-def home(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
 
 if __name__ == "__main__":
     import uvicorn
